67962a4dbc97ba05373f8d2cfefc788c37c526cb
venkata14 <venkata.patchigolla@gmail.com>
2024-08-04 13:30:18 -0400

    added database pocketbase hosting

---
+++ src/lib/database/pocketbase.js
@@ -0,0 +1,4 @@
+import Pocketbase from 'pocketbase'
+
+// export const pb = new Pocketbase("http://127.0.0.1:8090")
+export const pb = new Pocketbase("https://wshl.pockethost.io/")
--- src/routes/a/+server.js
+++ src/routes/a/+server.js
@@ -2,6 +2,7 @@ import { json } from '@sveltejs/kit'
 import twilio from 'twilio'
 import { TWILIO_ACCOUNTSID, TWILIO_AUTHTOKEN, TWILIO_NUMBER } from '$env/static/private';
 import { error } from '@sveltejs/kit'
+import { pb } from '$lib/database/pocketbase.js';
 
 let sid = TWILIO_ACCOUNTSID
 let auth = TWILIO_AUTHTOKEN
@@ -20,12 +21,31 @@ const d = {
     "pharmacy": "This is the Pharmacy Resource you requested from the WSHL Resource Bank!",
 }
 
+/**
+ * @param {string} text
+ */
+function extractUrlComp(text) {
+    const splitL = text.split('\n')
+    const lastLine = splitL[splitL.length - 1]
+    const reg = /(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/igm
+
+    const textURL = reg.exec(lastLine)
+    if (textURL) {
+        const textURLParts = textURL[0].split('/')
+        return textURLParts[textURLParts.length - 1]
+    } else {
+        return '<ERROR>'
+    }
+}
+
 // 8777804236
 async function sendSMS(content = "Sample", to = "+18606905005", devShutDown = true) {
     if (devShutDown) {
         console.log("API Shut Down")
         return false
     }
+
+    // return true
     const client = twilio(sid, auth)
 
     return client.messages
@@ -49,14 +69,24 @@ export async function POST(event) {
     const resourceToSend = data.resourceToSend
     const resourceTextDescription = data.resourceTextDescription
     // @ts-ignore
-    const content = `${d[resourceToSend]}
+    const content = `${d[resourceToSend] || "This is the resource you requested from the WSHL Resource Bank!"}
     
     ${resourceTextDescription}`
     const didTwilioWork = await sendSMS(content, number, false)
-    console.log(didTwilioWork)
+    // console.log(didTwilioWork)
 
     if (!didTwilioWork) {
         throw error(404, "API Shut Down, please use another service")
+    } else {
+        // Recording into DataBase
+        const data = {
+            timeRequested: new Date(),
+            resourceType: resourceToSend,
+            resourceName: extractUrlComp(content),
+            textSent: content
+        }
+        const newRecord = await pb.collection('use').create(data)
+        // console.log(newRecord)
     }
 
     return json({
